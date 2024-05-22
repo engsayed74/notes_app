d94bdb2e9ddf688ebcc16cccf688cc428f6370de
sayed <engsayed1993@gmail.com>
2024-05-22 18:32:49 +0300

    edit note done

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key,required this.title, required this.icon});
-final String title;
-final IconData icon;
-
+  const CustomAppBar(
+      {super.key, required this.title, required this.icon, this.onPressed});
+  final String title;
+  final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(title,
-        style:const TextStyle(fontSize: 28),),
-        CustomSearchIcon(icon: icon,)
+        Text(
+          title,
+          style: const TextStyle(fontSize: 28),
+        ),
+        CustomSearchIcon(
+          onPressed: onPressed,
+          icon: icon,
+        )
       ],
     );
   }
--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,24 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/views/widgets/cusom_text_field.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
-class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({super.key});
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
+  final NoteModel note;
 
   @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+   String? title,content;
+  @override
   Widget build(BuildContext context) {
-    return const Padding (
-      padding:  EdgeInsets.symmetric(horizontal: 24),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(height: 50,),
-          CustomAppBar(title: 'Edit Note',
-          icon: Icons.check,
+          const SizedBox(
+            height: 50,
           ),
-          SizedBox(height: 50,),
-          CustomTextField(hint: 'Title'),
-          SizedBox(height: 16,),
-          CustomTextField(hint: 'content',maxLines: 5,),
+          CustomAppBar(
+            onPressed: () {
+              widget.note.title=title ?? widget.note.title;
+              widget.note.subtitle=content ?? widget.note.subtitle;
+              widget.note.save();
+              BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+              Navigator.pop(context);
+            },
+            title: 'Edit Note',
+            icon: Icons.check,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+           CustomTextField(
+            hint: widget.note.title,
+            onChanged: (value) {
+              title=value;
+            },
+            ),
+          const SizedBox(
+            height: 16,
+          ),
+           CustomTextField(
+            onChanged: (value) {
+              content=value;
+              
+            },
+            hint: widget.note.subtitle,
+            maxLines: 5,
+          ),
         ],
       ),
     );
